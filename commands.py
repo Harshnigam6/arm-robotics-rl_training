d9a621976d64c52c77b77a7965349cb43721034b
Harsh  Nigam <harsh.nigam@pricelabs.co>
2025-04-04 13:03:05 +0530

    --------

--- commands.py
+++ commands.py
@@ -1,2 +1,7 @@
 salloc --time=1:0:0 --mem-per-cpu=16G --ntasks=1 --gres=gpu:1
 sbatch run_xarm6_training.sh --gpus-per-node=p100:1
+sbatch run_xarm6_training.sh --gpus-per-node=p100:1
+cat slurm-57986325.out
+
+cp run_xarm6_training run_xarm6_training.sh
+
